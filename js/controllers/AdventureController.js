924625166c86c5a3fa398c5ead10da64452cac89
bevinb <bgp816@msn.com>
2014-02-10 02:43:12 +0800

    update style

--- js/controllers/AdventureController.js
+++ js/controllers/AdventureController.js
@@ -1,6 +1,18 @@
 snappiq.controller('AdventureController', function ($scope, AdventureService) {
 	$scope.item = AdventureService.queryItem();
 	$scope.ratingPercent = $scope.item.rating / 5 * 100 + '%';
+	$scope.isCollapsed = false;
+	$scope.isInfoTabHidden = false;
+	$scope.imgViewerHeight = 320;
+	
+	$scope.toggleCollapse = function(){
+	    $scope.isCollapsed = !$scope.isCollapsed;
+		$scope.imgViewerHeight = $scope.isCollapsed? (document.body.clientHeight - 70): 320;
+	};
+	
+	$scope.toggleTab = function(label){
+	    $scope.isInfoTabHidden = label != "info";
+	};
 	
 	$scope.showRatingTips = function(n){
 	    $scope.ratingPercent = n / 10 * 100 + '%';
